1fbe556d6f9a324f9cec49f9ecde6307ca1c21e7
Willian Nunes de Oliveira <williangd@yahoo.com.br>
2016-11-17 22:54:11 -0200

    0406 structs

--- Ch04/04_06/structs.go
+++ Ch04/04_06/structs.go
@@ -4,6 +4,14 @@ import (
 	"fmt"
 )
 
-func main() {
+type Dog struct {
+	Breed  string
+	Weight int
+}
 
+func main() {
+	poodle := Dog{"Poodle", 34}
+	fmt.Println(poodle)
+	fmt.Printf("%+v\n", poodle)
+	fmt.Printf("Breed: %v\nWeigth: %v\n", poodle.Breed, poodle.Weight)
 }
